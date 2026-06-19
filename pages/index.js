309de9acc37b6bdeaa2cf4ff31b017b0e0318017
ehsan <ehsan217yaghoubi@gmail.com>
2026-06-19 23:14:49 +0330

    menu section ui completed

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Slider from "@/components/templates/Index/Slider";
 import About from "@/components/templates/Index/About";
 import Services from "@/components/templates/Index/Services";
 import Offer from "@/components/templates/Index/Offer";
+import Menu from "@/components/templates/Index/Menu";
 
 function Index({ data }) {
   return (
@@ -11,6 +12,7 @@ function Index({ data }) {
       <About />
       <Services services={data} />
       <Offer />
+      <Menu />
     </>
   );
 }
