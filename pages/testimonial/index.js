a9e8f39c4aae6f834147a5817636c96ae9bc401d
ehsan <ehsan217yaghoubi@gmail.com>
2026-06-24 21:42:37 +0330

    The Testimonail page was completed as ISR

--- pages/testimonial/index.js
+++ pages/testimonial/index.js
@@ -1,7 +1,26 @@
+import PageHeader from "@/components/modules/PageHeader/PageHeader";
+import Comments from "@/components/templates/Testimonial/Comments";
 import React from "react";
 
-function Testimonial() {
-  return <h1>Welcome to testimonial page</h1>;
+function Testimonial({ comments }) {
+  return (
+    <>
+      <PageHeader route="Testimonial" />
+      <Comments comments={comments} />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const commentsResponse = await fetch("http://localhost:4000/comments");
+  const commentsData = await commentsResponse.json();
+
+  return {
+    props: {
+      comments: commentsData,
+    },
+    revalidate: 43200,
+  };
 }
 
 export default Testimonial;
