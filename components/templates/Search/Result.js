785b8fe377a0513dad83ac659477b66b71927a8b
ehsan <ehsan217yaghoubi@gmail.com>
2026-06-25 23:56:05 +0330

    edit global search

--- components/templates/Search/Result.js
+++ components/templates/Search/Result.js
@@ -10,17 +10,21 @@ function Result({ data }) {
     <div className="container-fluid pt-5">
       <div className="container">
         <div className="row">
-          {categories.map((category) => (
-            <div key={category.type} className="col-lg-6">
-              <h1 className="mb-5">{category.title}</h1>
-              {data
-                .filter((menu) => menu.type === category.type)
-                .slice(0, 3)
-                .map((item) => (
-                  <Card key={item.id} {...item} />
-                ))}
-            </div>
-          ))}
+          {categories.map((category) => {
+            const filteredData = data.filter(
+              (item) => item.type === category.type,
+            );
+            return (
+              <div key={category.type} className="col-lg-6">
+                <h1 className="mb-5">{category.title}</h1>
+                {filteredData.length > 0 ? (
+                  filteredData.map((item) => <Card key={item.id} {...item} />)
+                ) : (
+                  <h4>No results found for {category.title}</h4>
+                )}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
